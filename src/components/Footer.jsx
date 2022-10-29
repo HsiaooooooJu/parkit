23a2d2b1b79ca4def6a1a989bf6638d2555cc53e
HsiaooooooJu <kelllllyko@gmail.com>
2022-10-29 20:45:50 +0800

    adjust: fix styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,6 +4,16 @@ export default function Footer() {
   return (
     <div className='footer-container'>
       <img src={logo} className='footer-container_logo' />
+      <div className='footer-container_block'>
+        <div className='footer-container_block_title'>停車</div>
+        <div className='footer-container_block_text'>快速導引</div>
+        <div className='footer-container_block_text'>地圖</div>
+      </div>
+      <div className='footer-container_block'>
+        <div className='footer-container_block_title'>設定</div>
+        <div className='footer-container_block_text'>地圖樣式</div>
+        <div className='footer-container_block_text'>關於我們</div>
+      </div>
     </div>
   )
 }
