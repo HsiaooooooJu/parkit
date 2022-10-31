973dc7cd0b222b6e27dda3f3844d313bcec289c2
HsiaooooooJu <kelllllyko@gmail.com>
2022-11-01 01:07:51 +0800

    adjust: fix SCSS

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,24 +8,26 @@ export default function Footer() {
         <Link to='/' className='footer-container_logo'>
           <img src={logo} />
         </Link>
-        
-        <div className='footer-container_block'>
-          <ul className='footer-container_block_title'>停車</ul>
-          <FooterLink to='/quick-nav' className='footer-container_block_text'>
-            快速導引
-          </FooterLink>
-          <FooterLink to='/parking' className='footer-container_block_text'>
-            停車地圖
-          </FooterLink>
-        </div>
-        <div className='footer-container_block'>
-          <ul className='footer-container_block_title'>設定</ul>
-          <FooterLink to='/map-style' className='footer-container_block_text'>
-            地圖樣式
-          </FooterLink>
-          <FooterLink to='/about-us' className='footer-container_block_text'>
-            關於我們
-          </FooterLink>
+
+        <div className="footer-container_wrap">
+          <div className='footer-container_block'>
+            <ul className='footer-container_block_title'>停車</ul>
+            <FooterLink to='/quick-nav' className='footer-container_block_text'>
+              快速導引
+            </FooterLink>
+            <FooterLink to='/parking' className='footer-container_block_text'>
+              停車地圖
+            </FooterLink>
+          </div>
+          <div className='footer-container_block'>
+            <ul className='footer-container_block_title'>設定</ul>
+            <FooterLink to='/map-style' className='footer-container_block_text'>
+              地圖樣式
+            </FooterLink>
+            <FooterLink to='/about-us' className='footer-container_block_text'>
+              關於我們
+            </FooterLink>
+          </div>
         </div>
       </div>
     </footer>
