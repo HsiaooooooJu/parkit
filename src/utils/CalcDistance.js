d0a027aebafcacac374c577fc64c475e028f00b3
HsiaooooooJu <kelllllyko@gmail.com>
2022-11-03 02:01:24 +0800

    feat: add utils for distance and attribution

---
+++ src/utils/CalcDistance.js
@@ -0,0 +1,2 @@
+// a(lat, lng) b(lat, lng)
+// (aLat + bLat)^2 + (bLng + bLng)^2 = distance
---
+++ src/utils/OsmProvider.js
@@ -0,0 +1,6 @@
+export default {
+  maptile : {
+    url: 'https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png',
+    attribution: '<a href="https://stadiamaps.com/">&copy; Stadia Maps</a><a href="https://openmaptiles.org/">&copy; OpenMapTiles</a><a href="http://openstreetmap.org">&copy; OpenStreetMap</a>'
+  }
+}
