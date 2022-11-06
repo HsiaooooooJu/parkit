6199ab1bacdacc34e8b9986955bf173b6ef3022d
HsiaooooooJu <kelllllyko@gmail.com>
2022-11-06 18:10:30 +0800

    feat: add map style

--- src/utils/OsmProvider.js
+++ src/utils/OsmProvider.js
@@ -1,6 +1,12 @@
 export default {
-  maptile : {
+  default: {
     url: 'https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png',
-    attribution: '<a href="https://stadiamaps.com/">&copy; Stadia Maps</a><a href="https://openmaptiles.org/">&copy; OpenMapTiles</a><a href="http://openstreetmap.org">&copy; OpenStreetMap</a>'
+    attribution:
+      '<a href="https://stadiamaps.com/">&copy; Stadia Maps</a><a href="https://openmaptiles.org/">&copy; OpenMapTiles</a><a href="http://openstreetmap.org">&copy; OpenStreetMap</a>'
+  },
+  tradition: {
+    url: 'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
+    attribution:
+      '<a href="https://www.openstreetmap.org/copyright">&copy OpenStreetMap</a> contributors <a href="https://carto.com/attributions">&copyCARTO</a>'
   }
 }
