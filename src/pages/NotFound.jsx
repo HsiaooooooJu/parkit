adfe556a2358cebd3937ab098ff46ab2b03b7e74
HsiaooooooJu <kelllllyko@gmail.com>
2022-11-08 12:04:20 +0800

    feat: update Not Found page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,23 @@
+import { Link } from 'react-router-dom'
+import cloudL from '../assets/images/cloud-l.svg'
+import cloudS from '../assets/images/cloud-s.svg'
+import arrow from '../assets/images/arrow.svg'
+import car from '../assets/images/car.svg'
+
 export default function NotFound() {
   return (
     <div className='not-found-container'>
-      <h1>Not Found</h1>
+      <div className='not-found'>
+        <img src={cloudL} className='not-found_cloud_large' />
+        <img src={cloudS} className='not-found_cloud_small' />
+        <h4 className='not-found_title'>404 Not Found</h4>
+        <img src={car} className='not-found_car' />
+        <div className='not-found_block'>
+          <Link to='/'>
+            <img src={arrow} className='not-found_block_arrow' />
+          </Link>
+        </div>
+      </div>
     </div>
   )
 }
