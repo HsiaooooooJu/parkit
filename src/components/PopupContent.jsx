5ba20f8a38eb1173188ca16fc8b9be4de4a9fba3
HsiaooooooJu <kelllllyko@gmail.com>
2022-11-17 01:19:11 +0800

    feat: add navigate btn

--- src/components/PopupContent.jsx
+++ src/components/PopupContent.jsx
@@ -1,9 +1,10 @@
+import navigate from '../assets/images/navigate.svg'
+
 export default function PopupContent(props) {
   return (
     <div className='popup'>
-      <a href={`https://www.google.com/maps/dir/?api=1&destination=${props.item.name}`} className='popup_title'>
-        {props.item.name}
-      </a>     <div className='popup_sub'>
+      <div className='popup_title'>{props.item.name}</div>
+      <div className='popup_sub'>
         <div className='popup_sub_box'>
           <span className='popup_sub_box_title'>空位</span>
           <span className='popup_sub_box_num'>{props.item.availableCar}</span>
@@ -13,22 +14,33 @@ export default function PopupContent(props) {
           <span className='popup_sub_box_num'>{props.item.totalCar}</span>
         </div>
       </div>
-      <div className='popup_detail_box'>
-        <h4 className='popup_detail_box_title'>費率</h4>
-        <span className='popup_detail_box_body'>{props.item.payex}</span>
+      <PopupBody item={props.item}>{{ title: '費率', text: props.item.payex }}</PopupBody>
+      <PopupBody item={props.item}>
+        {{ title: '營業時間', text: props.item.serviceTime }}
+      </PopupBody>
+      <PopupBody item={props.item}>
+        {{ title: '地址', text: props.item.address }}
+      </PopupBody>
+
+      <div className='popup_detail_group'>
+        <PopupBody item={props.item}>{{ title: '電話', text: props.item.tel }}</PopupBody>
+        <a
+          href={`https://www.google.com/maps/dir/?api=1&destination=${props.item.name}`}
+          className='popup_detail_group_btn'
+        >
+          <span className='popup_detail_group_btn_text'>Google 導航</span>
+          <img src={navigate} className='popup_detail_group_btn_img' />
+        </a>
       </div>
-      <div className='popup_detail_box'>
-        <h4 className='popup_detail_box_title'>營業時間</h4>
-        <span className='popup_detail_box_body'>{props.item.serviceTime}</span>
-      </div>
-      <div className='popup_detail_box'>
-        <h4 className='popup_detail_box_title'>地址</h4>
-        <span className='popup_detail_box_body'>{props.item.address}</span>
-      </div>
-      <div className='popup_detail_box'>
-        <h4 className='popup_detail_box_title'>電話</h4>
-        <span className='popup_detail_box_body'>{props.item.tel}</span>
-      </div>
+    </div>
+  )
+}
+
+function PopupBody({ children }) {
+  return (
+    <div className='popup_detail_box'>
+      <h4 className='popup_detail_box_title'>{children.title}</h4>
+      <span className='popup_detail_box_body'>{children.text}</span>
     </div>
   )
 }
